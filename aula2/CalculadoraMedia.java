050175fe0e6f3f977855e03ed9503fa3405752d7
eduardo <eduardojoo17@gmail.com>
2025-09-22 21:09:33 -0300

    22/09

---
+++ aula2/CalculadoraMedia.java
@@ -0,0 +1,10 @@
+public class CalculadoraMedia {
+    public static void main(String[] args) {
+        double nota1=5;
+        double nota2=6;
+        double nota3=9;
+        double nota4=7;
+
+        System.out.println("A media e: "+ (nota1+nota2+nota3+nota4)/4);
+    }
+}
---
+++ aula2/ExerciciosVariaveis.java
@@ -0,0 +1,12 @@
+public class ExerciciosVariaveis {
+    public static void main(String[] args) {
+        String nome ="João";
+        int idade = 20;
+        double peso = 52.5;
+        double altura = 1.75;
+
+        System.out.println("O funcionario "+ nome+ " tem: \nidade: "+idade+"\naltura: "+altura+"\npeso: "+peso);
+
+
+    }
+}
